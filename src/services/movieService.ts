c2efe24450ab72f128d30a5413ea967443ed9544
Yaro <olar13b@gmail.com>
2025-07-16 23:39:07 +0300

    all

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,19 +1,23 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-const API_URL = "https://api.th/emoviedb.org/3/search/movie";
+const API_URL = "https://api.themoviedb.org/3/search/movie";
 
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 interface FetchMoviesResponse {
   results: Movie[];
+  total_pages: number;
 }
 
-export const fetchMovies = (query: string): Promise<FetchMoviesResponse> => {
+export const fetchMovies = (
+  query: string,
+  page: number = 1
+): Promise<FetchMoviesResponse> => {
   const config = {
     params: {
       query,
-      page: 1,
+      page,
     },
     headers: {
       Authorization: `Bearer ${TOKEN}`,
